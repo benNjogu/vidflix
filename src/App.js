a168010fda2abed6f26eef5e8bcfbdaa3dcfb86c
benNjogu <kamandemachafi@gmail.com>
2022-09-18 19:50:25 +0300

    Like component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Movies from "./Components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World</h1>
+      <Movies />
     </main>
   );
 }
---
+++ src/Components/common/like.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+//liked: boolean
+//output: onClick
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      className={classes}
+      style={{ cursor: "pointer" }}
+      aria-hidden="true"
+    ></i>
+  );
+};
+
+export default Like;
