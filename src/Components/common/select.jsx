cc21d372857d690b9663a352113efc2f8ed37188
benNjogu <kamandemachafi@gmail.com>
2022-09-28 10:43:56 +0300

    Braces  error

--- src/Components/common/select.jsx
+++ src/Components/common/select.jsx
@@ -7,11 +7,11 @@ const Select = ({ name, label, options, error, ...rest }) => {
       <label htmlFor={name}>{label}</label>
       <select name={name} id={name} {...rest} className="form-control">
         <option value="" />
-        {options.map((option) => {
+        {options.map((option) => (
           <option key={option._id} value={option._id}>
             {option.name}
-          </option>;
-        })}
+          </option>
+        ))}
       </select>
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
