c8121c775aa5a250325e4dfcd8503602d0f006fd
benNjogu <kamandemachafi@gmail.com>
2022-11-13 11:58:17 +0300

    Refactoring

--- src/Components/movieForm.jsx
+++ src/Components/movieForm.jsx
@@ -27,20 +27,27 @@ class MovieForm extends Form {
       .label("Daily rental rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovies() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (error) {
       if (error.response && error.response.status === 404)
         return this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateGenres();
   }
 
   mapToViewModel(movie) {
